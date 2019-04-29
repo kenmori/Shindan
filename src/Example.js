c11755b49ddaeb603f3c36ec781ee0735a3b7bf9
kenjimorita <>
2019-04-29 09:53:26 +0900

    fix test

--- src/Example.js
+++ src/Example.js
@@ -9,7 +9,11 @@ const CountState = React.memo(({count}) => {
 const AnotherCountState = React.memo(({anotherCount}) => {
     return (<div>StateCount: {anotherCount.valueAnother}</div>);
 })
+
+
+
 export default () => {
+    const [lists, setLists] = useState([{name: "kenji", age: 19},{name: "morita", age: 20}, {name: "keiko", age: 21} ])
     const [count, setState] = useState({ value: 1 })
     const [anotherCount, setAnotherCount] = useState({valueAnother: 1});
     const [a, setA] = useState(0);
@@ -19,6 +23,16 @@ export default () => {
     const onPureChange = useCallback(() => {
         setA(a+1)
     },[a]);
+    const ListComponent = useMemo(() => (
+        lists.map((item, i) => {
+            return (
+                <>
+                <li>{item.name}</li>
+                <li>{item.age}</li>
+                </>
+            )
+        })
+        ), [lists])
     return (
         <div>
             <button onClick={()=> {
@@ -27,6 +41,7 @@ export default () => {
            <CountState count={count} />
            <AnotherCountState anotherCount={anotherCount}/>
             <ExampleComponent ClickFn={onPureChange} count={a} />
+            {ListComponent}
         </div>
     )
 }
